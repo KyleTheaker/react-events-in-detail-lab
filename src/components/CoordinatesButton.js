18337629ee942af40b94e7d9c6a4923c4c88df07
Kyle <kyle.theaker@yahoo.com>
2021-04-28 17:17:03 -0700

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,12 @@
+import React from "react";
+
+
 // Code CoordinatesButton Component Here
+ export default function CoordinatesButton(props) {
+     return(
+        <div>
+        <button onClick={(e) => props.onReceiveCoordinates([e.clientX, e.clientY])}>Coordinates</button>
+        </div>
+     )
+}
+
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,14 @@
+//import { ProgressPlugin } from "webpack";
+import React from 'react'
 // Code DelayedButton Component Here
+export default function DelayedButton(props){
+    return(
+        <div>
+            <button onClick={(e) => {
+                e.persist()
+                setTimeout(() => 
+                {props.onDelayedClick(e)}, props.delay)}}>x</button>
+        </div>
+    )
+}      
+
